98c3c38f670884e64387b11bf2efa3d34d660a7f
ngarg <easy@dhurv.com>
2018-07-06 22:51:01 -0500

    Design doc and other files added

---
+++ db/supervised.py
@@ -0,0 +1,11 @@
+
+from os import path
+from utils import fix_path
+
+def fx_supervised(v_uk_id):
+
+    base = Path(base)
+    Path(base / file_path)
+
+    return str(Path(base / file_path).absolute())
+
