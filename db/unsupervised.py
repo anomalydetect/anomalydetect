23da8a2cb59eac8effd7d21a7e1d82f90af50efa
ngarg <easy@dhurv.com>
2018-07-09 22:59:53 -0500

    another unsupervised commit

--- db/unsupervised.py
+++ db/unsupervised.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 from statsmodels import robust
 from statsmodels.stats.stattools import medcouple
-
+import json
 
 def fx_unsupervised(v_uk_id):
+	#v_uk_id= 123456
 	my_path = "data/" + v_uk_id + "/"
 	v_input_json_file = my_path + "details.json"
 
@@ -17,19 +18,32 @@ def fx_unsupervised(v_uk_id):
 	v_input_csv = my_path + input_json['filename']
 
 	df = pd.read_csv(v_input_csv)
-
+	df['anomalydetectid']=df.index
 	######################
-	v_id = input_json['id'][0]
-	v_fact = input_json['fact'][0]
-	v_analysis_columns_list = v_fact
-
-	v_analysis_columns = ''.join(v_analysis_columns_list)
-	df_outliers = fx_ThreeSigmaRule(df[v_id], df[v_analysis_columns], 2, 1)
-
-	df_outliers['Outlier Type'] = 'Three Sigma Rule'
-	df_outliers['Analysis Columns'] = v_analysis_columns
-
+	v_id = 'anomalydetectid'
+	v_analysis_columns_list = input_json['dimension'] 
+	df_outliers = pd.DataFrame({})
+	#v_analysis_columns = ''.join(v_analysis_columns_list)
+	for my_analysis_column in v_analysis_columns_list:
+		v_df_temp = fx_ThreeSigmaRule(df[v_id], df[my_analysis_column], 2, 1)
+		v_df_temp['Outlier Type'] = 'Three Sigma Rule'
+		v_df_temp['Analysis Column'] = my_analysis_column
+		df_outliers = pd.concat([df_outliers, v_df_temp])
 	######################
+	v_output_result_csv = my_path+"result_individual_columns.csv"
+	df_outliers.to_csv(v_output_result_csv, encoding="utf-8", index=False, header=True)
+
+	x_groupby_type = df1.groupby(['Analysis Column'])
+	df2 = x_groupby_type.count()
+	df2.reset_index(inplace=True)
+	df3 = df2.sort_values(['data'], ascending=True).head(4)
+
+	for i, r in df3.iterrows():
+		v_filename = 'image'+str(i)
+		v_list = df.loc[:, [r['Analysis Column']]]
+		v_title = 'Outliers for '+r['Analysis Column']
+		fx_box_plot (v_uk_id,v_list , v_title,v_filename )
+
 
 	return df_outliers
 
@@ -67,65 +81,78 @@ def fx_unsupervised_plot(v_uk_id):
 
 	return v_output_json_contents
 
-
+def fx_box_plot (v_uk_id,v_list , v_title,v_filename ):
+    my_path = "data/" + v_uk_id + "/"
+    fig, ax = plt.subplots()
+    jpg_filename = my_path + v_filename+'.jpg'
+    png_filename = my_path + v_filename+'.png'
+    x = range(len(v_list))
+    ax.boxplot(v_list, patch_artist=True)
+    ax.set_title(v_title)
+    fig.tight_layout()
+    #fig.show()
+    fig.savefig(jpg_filename, dpi=1000)
+    fig.savefig(png_filename+'.png', dpi=1000)
+	
 def fx_ThreeSigmaRule(series_id, series_data, v_number_of_std, v_masking_Iteration):
-    '''
-    value should be in +- range of -->mean + n * std
-    Probability
-    n =1 --> 68
-    n =2 -->  95
-    n= 3  --> 99.7
-    Usage : 
-    df_outliers= fx_ThreeSigmaRule(df['myid'],df['series1'], 2,2)
-    df_outliers
-    Good for normal distributed data ---series or sequences
-    Drawback :
-    Not good when data is not normal distribution 
-    Sensitive to extreme points  ---Masking effect. If extreme value is too large then it overshadow other values
-    -->iteration solves this issue
-    '''
+	'''
+	value should be in +- range of -->mean + n * std
+	Probability
+	n =1 --> 68
+	n =2 -->  95
+	n= 3  --> 99.7
+	Usage : 
+	df_outliers= fx_ThreeSigmaRule(df['myid'],df['series1'], 2,2)
+	df_outliers
+	Good for normal distributed data ---series or sequences
+	Drawback :
+	Not good when data is not normal distribution 
+	Sensitive to extreme points  ---Masking effect. If extreme value is too large then it overshadow other values
+	-->iteration solves this issue
+	'''
 
-    v_df = pd.DataFrame({})
-    v_df_outliers_final = pd.DataFrame({})
-    v_df['id'] = series_id
-    v_df['data'] = series_data
+	v_df = pd.DataFrame({})
+	v_df_outliers_final = pd.DataFrame({})
+	v_df['anomalydetectid'] = series_id
+	v_df['data'] = series_data
 
-    v_Iteration = 0
+	v_Iteration = 0
 
-    while (v_Iteration < v_masking_Iteration):
-        #############################
-        print(str(v_masking_Iteration))
-        v_masking_Iteration = v_masking_Iteration - 1
-        v_threshold = np.std(v_df['data']) * v_number_of_std
-        v_mean = np.mean(v_df['data'])
-        print(str(v_mean - v_threshold))
-        print(str(v_threshold + v_mean))
-        where_tuple = (np.abs(v_df['data'] - v_mean) > v_threshold)
-        v_df_outliers = v_df[where_tuple]
+	while (v_Iteration < v_masking_Iteration):
+		#############################
+		print(str(v_masking_Iteration))
+		v_masking_Iteration = v_masking_Iteration - 1
+		v_threshold = np.std(v_df['data']) * v_number_of_std
+		v_mean = np.mean(v_df['data'])
+		print(str(v_mean - v_threshold))
+		print(str(v_threshold + v_mean))
+		where_tuple = (np.abs(v_df['data'] - v_mean) > v_threshold)
+		v_df_outliers = v_df[where_tuple]
 
-        # v_outliersList = [ [r[0] , r[1]] for  i,r in v_df.iterrows() if np.abs(r[1]) > v_threshold + v_mean]
+		# v_outliersList = [ [r[0] , r[1]] for  i,r in v_df.iterrows() if np.abs(r[1]) > v_threshold + v_mean]
 
-        if (len(v_df_outliers) > 0):
+		if (len(v_df_outliers) > 0):
 
-            v_df_outliers_final = pd.concat([v_df_outliers_final, v_df_outliers])
+			v_df_outliers_final = pd.concat([v_df_outliers_final, v_df_outliers])
 
-            # Update data - remove otliers from the list
-            # list1 = [x for x in list1 if x not in v_outliersList[1]]
-            where_tuple = (np.abs(v_df['data'] - v_mean) <= v_threshold)
-            v_df = v_df[where_tuple]
+			# Update data - remove otliers from the list
+			# list1 = [x for x in list1 if x not in v_outliersList[1]]
+			where_tuple = (np.abs(v_df['data'] - v_mean) <= v_threshold)
+			v_df = v_df[where_tuple]
 
-        else:
+		else:
 
-            break
+			break
 
-        ############################
+		############################
 
-    if len(v_df_outliers_final) > 0:
+	if len(v_df_outliers_final) > 0:
 
-        return (v_df_outliers_final)
+		return (v_df_outliers_final)
 
-    else:
-        print("Three are No Outliers")
+	else:
+		return (pd.DataFrame({}))
+		print("Three are No Outliers")
 
 
 ###############################################################################
