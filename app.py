00c7fe58cfbd59d13f47403453638c882e80f9ec
Carlos Marin <carlos.marin@rackspace.com>
2018-07-11 20:41:17 -0500

    Fixes UI and adds backend functions to save JSON details and data.

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ app.config.from_object(config[config_name])
 
 #CORS(app, headers=['Content-Type'])
 
-
 def fx_uniqueid():
     seed = random.getrandbits(32)
     while True:
@@ -39,50 +38,74 @@ def fx_uniqueid():
        seed += 1
 ######################################################
 
+ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def create_upload_folder():
+	v_unique_id = str(next(fx_uniqueid()))
+	v_upload_folder = app.config['UPLOAD_BASE'] + v_unique_id
+	os.makedirs(v_upload_folder)
+	print("I am in upload route", file=sys.stderr)
+	print("I am in upload route", file=sys.stdout)
+	print("Created new folder: " + v_upload_folder)
+	return v_upload_folder
+
+
 @app.route('/submit_form', methods=['GET', 'POST'])
 def model_upload():
 	print("I am in submit_form route with post", file=sys.stderr)
 	print("I am in submit_form route with post", file=sys.stdout)
+
 	if request.method == 'POST':
+    
+		v_upload_folder = create_upload_folder()
+		v_output_data_file = v_upload_folder + "/" + "custom_anomaly_data.csv"
+		v_output_json_file = v_upload_folder + "/" + "details.json"
+
+		if request.files.get('file'):
+			file = request.files['file']
+			file.save(v_output_data_file)
+		else:
+			error_msg = "Invalid submission - missing file."
+			return render_template('index.html', error_msg=error_msg)
+
+		# get form values
 		time_series = request.form.get('time-series')
 		dimension = request.form.get('dimension')
 		label = request.form.get('label')
 		fact = request.form.get('fact')
-		model = request.form.get('model')
+		learning_type = request.form.get('learning_type')
 
 		# to debug
+		print("Got form values:")
 		form = dict(request.form)
 		for k, v in form.items():
 			print(str(k) + ":" + str(v))
 
-		# call model, do magic and return to template
-		# v_my_id should be supplied here 
-		v_my_id = app.config['UNIQUE_ID']
-		fx_analysis(v_my_id)
-		
-		return ('Got: time_series={}, dimension={}, label={}, fact={}, model={}'
-				.format(id, time_series, dimension, label, fact, model))
-
-
-		my_path = app.config['UPLOAD_FOLDER'] + "/"
-		v_output_json_file = my_path + "details.json"
 
 		##################################json code
 		v_output_json_contents = {
-		"filename":  "custom_anomaly_data.csv" ,
-		"label":  ["label"] ,
-		"time_series":  ["series1"]  ,
-		"dimension":  ["dim1_v1" , "dim1_v2" , "dim2_v1" , "dim2_v2","dim2_v3"]  ,
-		"fact":  ["fact2" , "fact1"]  ,
-		"id":  ["myid"]  ,
-		"learning_type" : ["unsupervised"]
+			"filename":  v_output_data_file,
+			"fact": fact,
+			"label": label,
+			"dimension": dimension,
+			"time_series": time_series,
+			"learning_type" : learning_type,
 		}
 
-		   
+		for k, v in v_output_json_contents.items():
+			if(not v):
+				error_msg = "Invalid submission - missing value for: '" + str(k) + "'"
+				return render_template('index.html', error_msg=error_msg) 
 
 		with open(v_output_json_file, 'w') as outfile:
-		   json.dump(v_output_json_contents, outfile)
+			json.dump(v_output_json_contents, outfile)
 
+		# call model fucntions to create images
+		return render_template("result.html")
 
 	else:
 		return "Nothing to see here."
@@ -100,38 +123,9 @@ def home():
 
 #########################################
 
-@app.route('/upload')
-def upload_complete():
-
-	"""Saves imported folder to server"""
-	### set upload folder 
-	unique_sequence = fx_uniqueid()
-	my_id = next(unique_sequence)
-	app.config['UNIQUE_ID'] = str(my_id)
-	v_unique_id = app.config['UNIQUE_ID']
-	v_upload_folder = app.config['UPLOAD_BASE'] + app.config['UNIQUE_ID'] 
-	app.config['UPLOAD_FOLDER'] = v_upload_folder 
-	### 
-	os.makedirs(app.config['UPLOAD_FOLDER'])
-		
-	print("I am in upload route", file=sys.stderr)
-	print("I am in upload route", file=sys.stdout)
-
-	print(app.config['UPLOAD_FOLDER'])
-
-	ALLOWED_EXTENSIONS = set(['csv'])
-
-	return render_template("index.html" , v_upload_folder = v_upload_folder , v_unique_id = v_unique_id)
-
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-	
+
 	print("I am in root route with post", file=sys.stderr)
 	print("I am in root route with post", file=sys.stdout)
 	if request.method == 'POST':
@@ -151,7 +145,7 @@ def upload_file():
 			return redirect(url_for('uploaded_file',
 									filename=filename))
 	return ''
-	
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -171,13 +165,13 @@ def submit_complete(v_uk_id):
 	"""
 	print("I am in submit/uk route", file=sys.stderr)
 	print("I am in submit/uk route", file=sys.stdout)
-	
+
 	v_status = fx_result(v_uk_id)
-	
+
 	v_upload_folder = app.config['UPLOAD_BASE'] + v_uk_id
 	v_result_csv_url = v_upload_folder + '/result.csv'
-	
-	
+
+
 	return render_template("result.html", v_result_csv_url=v_result_csv_url , v_status = v_status)
 
 
